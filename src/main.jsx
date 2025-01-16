77434718dd247d1413f6402a321f4111e90e4087
Vicente Duch <vhduch@gmail.com>
2025-01-16 11:14:55 +0100

    Redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,13 @@ import App from './App.jsx'
 
 import './i18n.js'
 
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </StrictMode>,
 )
