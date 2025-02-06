95e49bc5b0c473b0c42ec4976ed48aeba36334c1
Vicente Duch <vhduch@gmail.com>
2025-02-06 12:12:25 +0100

    add Links and routing to Example View

--- src/views/ExampleView/ExampleView.jsx
+++ src/views/ExampleView/ExampleView.jsx
@@ -1,9 +1,13 @@
 import { ExampleSection } from './ExampleSection';
+import { Link } from 'react-router-dom';
 
 const ExampleView = () => {
 
     return (
         <div>
+            <Link to="/task-board-test">
+                <button>Go to test task board</button>
+            </Link>
             <ExampleSection />
         </div>
     );
--- src/views/ExampleView/__tests__/ExampleView.test.jsx
+++ src/views/ExampleView/__tests__/ExampleView.test.jsx
@@ -3,11 +3,14 @@ import '@testing-library/jest-dom';
 import { ExampleView } from '../';
 import i18next from '../../../i18n';
 import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('ExampleView', () => {
     it('should render the view', () => {
         render(<I18nextProvider i18next={i18next}>
-            <ExampleView />
+            <BrowserRouter>
+                <ExampleView />
+            </BrowserRouter>
         </I18nextProvider>);
         const button = screen.getByText('Click me');
         expect(button).toBeInTheDocument();
