868d0d0da0aa72174e4eb555c87d0c2a53329473
Vicente Duch <vhduch@gmail.com>
2025-02-26 11:31:13 +0100

    add conditional style to progressbar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -2,13 +2,18 @@ import "./ProgressBar.css";
 
 export const ProgressBar = ({ completedTasks, totalTasks }) => {
     const progress = totalTasks === 0 ? 0 : (completedTasks / totalTasks) * 100;
+    const progressBarColor = progress === 100? "green" : "#40c2ee";
 
     return (
         <div className="progress-bar-container">
-            <div className="progress-bar" style={{ width: `${progress}%` }}>
+            <div className="progress-bar" style={{ 
+                width: `${progress}%` ,
+                backgroundColor: progressBarColor
+            }}>
+                <span className="progress-text">
+                    {Math.round(progress)}%
+                </span>
             </div>
-            <p className="progress-text">{completedTasks} / {totalTasks}</p>
-
         </div>
     )
 }
